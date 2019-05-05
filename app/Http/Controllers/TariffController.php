2289928892d938a2668e2eac1c073db365147580
hak055 <hak055-sahakyan@yandex.ru>
2019-05-05 13:06:28 +0300

    mail send

--- app/Http/Controllers/TariffController.php
+++ app/Http/Controllers/TariffController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\SendEmail;
+use Illuminate\Support\Facades\Mail;
 
 class TariffController extends Controller
 {
@@ -12,6 +14,7 @@ class TariffController extends Controller
 	}
     public function index()
     {
+    	Mail::to('hak055-sahakyan@yandex.ru')->send(new SendEmail);
     	return view('tariff');
     }
 }
